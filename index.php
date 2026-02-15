1e4368364edd6f1bfbd34197f9c84e908788c96c
Damota38 <a.damota.dw@gmail.com>
2026-02-15 13:03:36 +0100

    fix: index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'db.php';
 
-$sql = "SELECT * FROM cars ORDER BY manufactrer, model";
+$sql = "SELECT * FROM cars ORDER BY manufacturer, model";
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
@@ -23,7 +23,16 @@ $cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <main>
         <div class="container">
+            <?php foreach ($cars as $car): ?>
+                <div class = "card">
+                    <h1><?php echo($car['manufacturer'] . ' ' . $car['model']); ?></h1>
+                    <p>Date: <?php echo($car['release_date']); ?></p>
+                    <p>Type: <?php echo($car['type']); ?></p>
+                    <p>Transmission: <?php echo($car['transmission']); ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>              
 
-    
 </body>
 </html>
